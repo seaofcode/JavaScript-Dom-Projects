9f627d6c827336ec875808af681d41ce497aee57
Matthew J Pearson <seaofcode@gmail.com>
2022-07-19 12:46:05 -0500

    Smooth Scroll Done

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -51,3 +51,31 @@ window.addEventListener('scroll', function(){
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(function(link){
+    link.addEventListener("click", function(e){
+        // prevent default
+        e.preventDefault();
+        // navigate to specific spot
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+        // calc the heights
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains('fixed-nav');
+        let position = element.offsetTop - navHeight;
+        if (!fixedNav) {
+            position = position - navHeight;
+        }
+        if (navHeight > 82) {
+            position = position + containerHeight;
+        }
+
+        window.scrollTo({
+            left:0,
+            top:position,
+        });
+        linksContainer.style.height = 0;
+    });
+});
