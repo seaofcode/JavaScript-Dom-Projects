8c77d8f1592bbb4652b9664ef217b34907ccc539
Matthew J Pearson <seaofcode@gmail.com>
2022-07-19 12:23:09 -0500

    nav bar and button done

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -7,8 +7,47 @@
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
 
+navToggle.addEventListener('click', function(){
+    // simple setup hardcoded value
+    // linksContainer.classList.toggle('show-links');
+
+    // dynamic calc
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    }else{
+        linksContainer.style.height = 0;
+    }
+});
+
+
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
 // ********** fixed navbar ************
+window.addEventListener('scroll', function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav');
+    } else {
+        navbar.classList.remove('fixed-nav');
+    }
+
+    // show back to top button
+    if (scrollHeight > 500) {
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove('show-link');
+    }
+});
+
+
 
 // ********** smooth scroll ************
 // select links
