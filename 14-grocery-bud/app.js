f448ab3976f641bdeb94cb90c8072707ea147f95
Matthew J Pearson <seaofcode@gmail.com>
2022-08-04 14:02:56 -0500

    adding items to list

--- 14-grocery-bud/app.js
+++ 14-grocery-bud/app.js
@@ -21,16 +21,63 @@ function addItem(e){
     e.preventDefault();
     const value = grocery.value;
     const id = new Date().getTime().toString();
-    if(value !== '' && editFlag === false){
-        console.log('add item to the list');
+
+    if(value && !editFlag){
+        const element = document.createElement('article');
+        // add class
+        element.classList.add('grocery-item');
+        // add id
+        const attr = document.createAttribute('data-id');
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>`;
+            // append child
+            list.appendChild(element);
+            // display alert
+            displayAlert('item added to the list', 'success');
+            // show container
+            container.classList.add('show-container');
+            // add to local storage
+            addToLocalStorage(id, value);
+            // set back to default
+            setBackToDefault();
     }
-    else if(value !== '' && editFlag === true){
+    else if(value && editFlag){
         console.log('editing');
     }
     else{
-        console.log('empty value');
+        displayAlert('please enter value', "danger");
     }
 }
+
+// display alert
+function displayAlert(text, action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    // remove alert
+    setTimeout(function(){
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+    }, 1000);
+}
+
+// set back to default
+function setBackToDefault() {
+    console.log('back to default');
+}
+
 // ****** LOCAL STORAGE **********
+function addToLocalStorage(id, value){
+    console.log('added to local storage');
+}
 
 // ****** SETUP ITEMS **********
