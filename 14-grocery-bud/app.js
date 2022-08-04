4eca3fb0dc62df8b05f609162bdd106842b3c814
Matthew J Pearson <seaofcode@gmail.com>
2022-08-04 13:44:02 -0500

    Update app.js
    started Submit Event Listener

--- 14-grocery-bud/app.js
+++ 14-grocery-bud/app.js
@@ -13,9 +13,24 @@ let editFlag = false;
 let editID = "";
 
 // ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener('submit', addItem)
 
 // ****** FUNCTIONS **********
-
+function addItem(e){
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+    if(value !== '' && editFlag === false){
+        console.log('add item to the list');
+    }
+    else if(value !== '' && editFlag === true){
+        console.log('editing');
+    }
+    else{
+        console.log('empty value');
+    }
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
