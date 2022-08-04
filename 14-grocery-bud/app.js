ce0f4fa01bab2b873e1eae0dfc9a107c259723e4
Matthew J Pearson <seaofcode@gmail.com>
2022-08-04 16:09:09 -0500

    Update app.js
    add to local storage

--- 14-grocery-bud/app.js
+++ 14-grocery-bud/app.js
@@ -133,7 +133,12 @@ function setBackToDefault() {
 
 // ****** LOCAL STORAGE **********
 function addToLocalStorage(id, value){
-    console.log('added to local storage');
+    const grocery = {id, value};
+    let items = localStorage.getItem('list')
+    ? JSON.parse(localStorage.getItem('list'))
+    : [];
+    items.push(grocery);
+    localStorage.setItem('list', JSON.stringify(items));
 }
 
 
